80759165c9c74ac4d4f6cb260ecf2d19ea0f1903
Gary Ellingson <gary.ellingson@asirobots.com>
2019-12-04 16:20:06 -0700

    fixed on bug in the ekf code and then mostly visualization to see the states in P decreasing with propogation only

--- ekf.py
+++ ekf.py
@@ -3,12 +3,12 @@ import numpy as np
 class EKF:
     def __init__(self, x0=-5, y0=-3, th0=np.pi/2, alpha=[0.1, 0.01, 0.01, 0.1], lm=[(6,4),(-7,8),(6,-4)], sigR=0.1, sigB=0.05):
         self.x = np.array([[x0],[y0],[th0]])
-        self.P = np.diag([2,2,np.pi/4])
+        self.P = np.diag([0,0,0*np.pi/4])
         self.alpha = alpha
         self.lm = lm
         self.R = np.diag([sigR,sigB])
 
-    def propodate(self, u, dt):
+    def propogate(self, u, dt):
         v = u[0,0]
         w = u[1,0]
         theta = self.x[2,0]
@@ -16,15 +16,16 @@ class EKF:
                       [0,1,v/w*(-np.sin(theta) + np.sin(theta + w*dt))],
                       [0,0,1]])
         G = np.array([[(-np.sin(theta) + np.sin(theta + w*dt))/w, v/w*((np.sin(theta) - np.sin(theta + w*dt))/w + np.cos(theta + w*dt)*dt)],
-                      [(np.cos(theta) - np.cos(theta + w*dt))/w, v/w*((-np.cos(theta) - np.cos(theta + w*dt))/w + np.sin(theta + w*dt)*dt)],
+                      [(np.cos(theta) - np.cos(theta + w*dt))/w, v/w*((-np.cos(theta) + np.cos(theta + w*dt))/w + np.sin(theta + w*dt)*dt)],
                       [0,dt]])
         Qu = np.diag([(self.alpha[0]*np.abs(v) + self.alpha[1]*np.abs(w))**2, (self.alpha[2]*np.abs(v) + self.alpha[3]*np.abs(w))**2])
         self.x += np.array([[v/w*(-np.sin(theta) + np.sin(theta + w*dt))],
                             [v/w*(np.cos(theta) - np.cos(theta + w*dt))],
                             [w*dt]])
+        R = np.diag([[0.001, 0.001, 0.00001]])
 
         # print A.shape, self.P.shape, G.shape, Qu.shape
-        self.P = A.dot(self.P).dot(A.T) + G.dot(Qu).dot(G.T)
+        self.P = A.dot(self.P).dot(A.T) + G.dot(Qu).dot(G.T) #+ R
 
     def update(self, z):
         for i in range(len(self.lm)):
@@ -45,3 +46,7 @@ class EKF:
 
     def vars(self):
         return np.array([[np.sqrt(self.P[0,0])], [np.sqrt(self.P[1,1])], [np.sqrt(self.P[2,2])]])
+
+    def state_elps(self):
+        vals, vecs = np.linalg.eig(self.P[0:2,0:2])
+        return 4*np.sqrt(vals[0]), 4*np.sqrt(vals[1]), np.arctan2(vecs[1,0],vecs[0,0])
--- framework.py
+++ framework.py
@@ -3,9 +3,11 @@ from rover import Rover
 from ekf import EKF
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.patches import Ellipse
 
 ts = 0.1
 num_st = 3
+x0 = np.array([[-5], [-3], [np.pi/2]])
 
 if __name__ == '__main__':
     rov = Rover()
@@ -16,32 +18,55 @@ if __name__ == '__main__':
     gain = np.array([]).reshape(num_st,0)
     time = np.arange(0, 20, ts)
 
-    plt.figure(0)
-    plt.axis([-10, 10, -10, 10])
-    plt.scatter([6,-7,6],[4,8,-4])
+    # plt.figure(0)
+    # plt.axis([-10, 10, -10, 10])
+    # plt.scatter([6,-7,6],[4,8,-4])
     plt.ion()
 
     for t in time:
-        u = np.array([[1 + 0.5*np.cos(2*np.pi*0.2*t)], [-0.2 + 2*np.cos(2*np.pi*0.6*t)]])
+        #u = np.array([[1 + 0.5*np.cos(2*np.pi*0.2*t)], [-0.2 + 2*np.cos(2*np.pi*0.6*t)]])
+        u = np.array([[1.5], [-0.3]])
 
         rov.propogate_dynamics(u, ts)
         truth = np.concatenate((truth, rov.true_state()),axis=1)
 
-        plt.clf()
-        plt.axis([-10, 10, -10, 10])
+        # plt.figure(0)
+        # plt.clf()
+        # plt.axis([-10, 10, -10, 10])
         [[x],[y],[t]] = rov.true_state()
-        plt.scatter(x, y)
-        plt.scatter(x + 0.5*np.cos(t), y + 0.5*np.sin(t), color='r')
-        mes = rov.get_mesurement()
-        for i in range(3):
-            r = mes[2*i,0]
-            b = mes[2*i + 1,0]
-            plt.scatter(x + r*np.cos(t+b), y + r*np.sin(t+b), color='g')
-        plt.scatter([6,-7,6],[4,8,-4])
-        # plt.pause(0.00001)
+        # plt.scatter(x, y)
+        # plt.scatter(x + 0.5*np.cos(t), y + 0.5*np.sin(t), color='r')
+        # mes = rov.get_mesurement()
+        # for i in range(3):
+        #     r = mes[2*i,0]
+        #     b = mes[2*i + 1,0]
+        #     plt.scatter(x + r*np.cos(t+b), y + r*np.sin(t+b), color='g')
+        # plt.scatter([6,-7,6],[4,8,-4])
 
-        ekf.propodate(u, ts)
-        gain =  np.concatenate((gain, ekf.update(rov.get_mesurement())), axis=1)
+
+        plt.figure(1)
+        plt.clf()
+        a = plt.subplot(111, aspect='equal')
+        st = ekf.est_state()
+        e1, e2, ang = ekf.state_elps()
+        e = Ellipse((st[0,0], st[1,0]), e1, e2, (ang + x0[2,0])*180/np.pi)
+        a.add_artist(e)
+        e.set_alpha(0.5)
+        e.set_facecolor([1.,0.,0.])
+
+        e = Ellipse((x,y),0.15,0.15,0)
+        a.add_artist(e)
+        e.set_facecolor([1.,0.,0.])
+
+        plt.plot(truth[0,:], truth[1,:])
+        plt.plot(estimate[0,:], estimate[1,:])
+
+        plt.axis([-10, 10, -10, 10])
+
+        plt.pause(0.00001)
+
+        ekf.propogate(u, ts)
+        # gain =  np.concatenate((gain, ekf.update(rov.get_mesurement())), axis=1)
         estimate = np.concatenate((estimate, ekf.est_state()), axis=1)
         variance = np.concatenate((variance, ekf.vars()), axis=1)
 
@@ -72,101 +97,37 @@ if __name__ == '__main__':
     plt.subplot(311)
     plt.title("Truth vs Estimate")
     plt.plot(time, truth[0,:] - estimate[0,:], label='error')
-    plt.plot(time, 2*variance[0,:], label='std div (2sig)')
-    plt.plot(time, -2*variance[0,:], label='std div (2sig)')
+    plt.plot(time, 2*variance[0,:], 'r', label='2 sig')
+    plt.plot(time, -2*variance[0,:], 'r', label='2 sig')
     plt.legend()
     # plt.xlabel("time")
     plt.ylabel("x")
     plt.subplot(312)
     plt.plot(time, np.abs(truth[1,:] - estimate[1,:]), label='error')
-    plt.plot(time, 2*variance[1,:], label='std div (2sig)')
-    plt.plot(time, -2*variance[1,:], label='std div (2sig)')
+    plt.plot(time, 2*variance[1,:], 'r', label='2 sig')
+    plt.plot(time, -2*variance[1,:], 'r', label='2 sig')
     # plt.legend()
     # plt.xlabel("time")
     plt.ylabel("y")
     plt.subplot(313)
     plt.plot(time, np.abs(truth[2,:] - estimate[2,:]), label='error')
-    plt.plot(time, 2*variance[2,:], label='std div (2sig)')
-    plt.plot(time, -2*variance[2,:], label='std div (2sig)')
+    plt.plot(time, 2*variance[2,:], 'r', label='2 sig')
+    plt.plot(time, -2*variance[2,:], 'r', label='2 sig')
     # plt.legend()
     plt.xlabel("Time")
     plt.ylabel("theta (rad)")
 
-    plt.figure(3)
-    plt.subplot(311)
-    plt.title("Kalman Gain")
-    plt.plot(time, gain[0,:])
-    plt.ylabel("x gain")
-    plt.subplot(312)
-    plt.plot(time, gain[1,:])
-    plt.ylabel("y gain")
-    plt.subplot(313)
-    plt.plot(time, gain[2,:])
-    plt.xlabel("Time")
-    plt.ylabel("theta gain")
+    # plt.figure(3)
+    # plt.subplot(311)
+    # plt.title("Kalman Gain")
+    # plt.plot(time, gain[0,:])
+    # plt.ylabel("x gain")
+    # plt.subplot(312)
+    # plt.plot(time, gain[1,:])
+    # plt.ylabel("y gain")
+    # plt.subplot(313)
+    # plt.plot(time, gain[2,:])
+    # plt.xlabel("Time")
+    # plt.ylabel("theta gain")
 
     plt.show()
-
-    # import scipy.io as spio
-    #
-    # kf = KF(ts)
-    # mat = spio.loadmat('hw1_soln_data.mat')
-    # vtr = mat['vtr']
-    # xtr = mat['xtr']
-    # t = mat['t']
-    # kf.x = mat['mu0']
-    # kf.P = mat['Sig0']
-    # u = mat['u']
-    # z = mat['z']
-    #
-    # estimate = np.array([]).reshape(2,0)
-    # variance = np.array([]).reshape(2,0)
-    # gain = np.array([]).reshape(2,0)
-    #
-    # for i in range(t.shape[1]):
-    #     kf.propodate(u[0,i])
-    #     gain =  np.concatenate((gain, kf.update(z[0,i])), axis=1)
-    #     estimate = np.concatenate((estimate, kf.est_state()), axis=1)
-    #     variance = np.concatenate((variance, kf.vars()), axis=1)
-    #
-    # plt.figure(1)
-    # plt.subplot(211)
-    # plt.title("States")
-    # plt.plot(t[0,:], xtr[0,:], label='truth')
-    # plt.plot(t[0,:], estimate[1,:], label='estimate')
-    # plt.legend()
-    # # plt.xlabel("Time")
-    # plt.ylabel("Possition (m)")
-    # plt.subplot(212)
-    # plt.plot(t[0,:], vtr[0,:], label='truth')
-    # plt.plot(t[0,:], estimate[0,:], label='estimate')
-    # # plt.legend()
-    # plt.xlabel("Time")
-    # plt.ylabel("Velocity (m/s)")
-    #
-    # plt.figure(2)
-    # plt.subplot(211)
-    # plt.title("Error")
-    # plt.plot(t[0,:], np.abs(xtr[0,:] - estimate[1,:]), label='error')
-    # plt.plot(t[0,:], 3*variance[1,:], label='std div (3sig)')
-    # plt.legend()
-    # # plt.xlabel("Time")
-    # plt.ylabel("Possition (m)")
-    # plt.subplot(212)
-    # plt.plot(t[0,:], np.abs(vtr[0,:] - estimate[0,:]), label='error')
-    # plt.plot(t[0,:], 3*variance[0,:], label='vel cov (3sig)')
-    # # plt.legend()
-    # plt.xlabel("Time")
-    # plt.ylabel("Velocity (m/s)")
-    #
-    # plt.figure(3)
-    # plt.subplot(211)
-    # plt.title("Kalman Gain")
-    # plt.plot(t[0,:], gain[1,:])
-    # plt.ylabel("Possition gain")
-    # plt.subplot(212)
-    # plt.plot(t[0,:], gain[0,:])
-    # plt.xlabel("Time")
-    # plt.ylabel("Velocity gain")
-    #
-    # plt.show()
